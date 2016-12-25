57083f14ffcb9ba54b10400cd13339b373364cb0
AldarTron <aldartron@gmail.com>
2016-12-25 12:54:51 +0900

    Start method added

--- grid.py
+++ grid.py
@@ -1,11 +1,14 @@
 import pygame
 from pygame import Surface
 from cell import Cell
+import time
+import threading
 
 
 class Grid:
 
     def __init__(self, size, screen, settings):
+        self.live = False
         self.is_deleting = False
         self.screen = screen
         self.settings = settings
@@ -32,6 +35,23 @@ class Grid:
                 pygame.draw.line(self.screen, self.settings.grid_color, (0, i * cell_size), (screen_height, i * cell_size))
                 pygame.draw.line(self.screen, self.settings.grid_color, (i * cell_size, 0), (i * cell_size, screen_height))
 
+    def do_epoch(self):
+        # Эпоха
+        for row in self.grid:
+            for cell in row:
+                if self.count(cell) == 3:
+                    cell.alive = True
+                elif self.count(cell) == 2 and cell.alive:
+                    cell.alive = True
+                else: cell.alive = False
+        print('epoch')
+
+    def do_live(self):
+        # Генерация эпох
+        while (self.live):
+            self.do_epoch()
+            time.sleep(1)
+
     def clear(self):
         # Очистка грида
         for row in self.grid:
@@ -46,5 +66,31 @@ class Grid:
 
     def start(self):
         # Запуск
-        pass
+        self.live = True
+        t = threading.Thread(target = self.do_live())
+        t.start()
+        t.join()
+
+    def count(self, cell):
+        # Подсчет количества живых соседей у клетки
+        coord_deltas = [(-1,-1),(0,-1),(+1,-1),(-1,0),(+1,0),(-1,+1),(0,+1),(+1,+1)]
+        neighbors = []
+        result = 0
+
+        for t in coord_deltas:
+            x = list(range(self.settings.grid_size))[(cell.x + t[0]) % self.settings.grid_size]
+            y = list(range(self.settings.grid_size))[(cell.y + t[1]) % self.settings.grid_size]
+            neighbor = (x, y)
+            neighbors.append(neighbor)
+
+        for coord in neighbors:
+            for row in self.grid:
+                for cell in row:
+                    if cell.x == coord[0] and cell.y == coord[1]:
+                        if cell.alive:
+                            result += 1
+
+        return result
+
+
 
