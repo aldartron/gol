c1e1cf180ec6fa17c0d57f5ec0a93e9cff1ccf8a
AldarTron <aldartron@gmail.com>
2017-01-03 19:57:40 +0900

    game mechanic fixed; vizualization display epoches now

--- grid.py
+++ grid.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame import Surface
 from cell import Cell
 import time
 import threading
@@ -34,23 +33,31 @@ class Grid:
             for i in range(self.settings.grid_size + 1):
                 pygame.draw.line(self.screen, self.settings.grid_color, (0, i * cell_size), (screen_height, i * cell_size))
                 pygame.draw.line(self.screen, self.settings.grid_color, (i * cell_size, 0), (i * cell_size, screen_height))
+        if self.live:
+            self.do_live()
 
     def do_epoch(self):
         # Эпоха
+        must_die = []
+        must_live = []
         for row in self.grid:
             for cell in row:
                 if self.count(cell) == 3:
-                    cell.alive = True
+                    must_live.append(cell)
                 elif self.count(cell) == 2 and cell.alive:
-                    cell.alive = True
-                else: cell.alive = False
+                    must_live.append(cell)
+                else:
+                    must_die.append(cell)
+        for dead in must_die:
+            dead.change(False)
+        for live in must_live:
+            live.change(True)
         print('epoch')
 
     def do_live(self):
         # Генерация эпох
-        while (self.live):
-            self.do_epoch()
-            time.sleep(1)
+        self.do_epoch()
+        time.sleep(1)
 
     def clear(self):
         # Очистка грида
@@ -67,30 +74,25 @@ class Grid:
     def start(self):
         # Запуск
         self.live = True
-        t = threading.Thread(target = self.do_live())
-        t.start()
-        t.join()
 
     def count(self, cell):
         # Подсчет количества живых соседей у клетки
-        coord_deltas = [(-1,-1),(0,-1),(+1,-1),(-1,0),(+1,0),(-1,+1),(0,+1),(+1,+1)]
+        coord_deltas = [(-1,-1),(0,-1),(1,-1),
+                        (-1, 0),       (1, 0),
+                        (-1, 1),(0, 1),(1, 1)]
         neighbors = []
         result = 0
 
         for t in coord_deltas:
-            x = list(range(self.settings.grid_size))[(cell.x + t[0]) % self.settings.grid_size]
-            y = list(range(self.settings.grid_size))[(cell.y + t[1]) % self.settings.grid_size]
-            neighbor = (x, y)
-            neighbors.append(neighbor)
+            x = cell.x + t[0]
+            y = cell.y + t[1]
+            neighbors.append((x,y))
 
-        for coord in neighbors:
+        for coords in neighbors:
             for row in self.grid:
                 for cell in row:
-                    if cell.x == coord[0] and cell.y == coord[1]:
+                    if cell.x == coords[0] and cell.y == coords[1]:
                         if cell.alive:
                             result += 1
 
         return result
-
-
-
